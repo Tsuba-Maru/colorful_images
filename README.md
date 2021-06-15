df53cd40e19af8c84eb7c33f331c55ad54e7f6d2
TsubasaMaruyama <future1105@ezweb.ne.jp>
2021-06-15 19:24:50 +0900

    change test image

--- README.md
+++ README.md
@@ -4,8 +4,14 @@
 file：元の画像ファイル名。拡張子まで含む。
 num_img：生成する画像の枚数
 save_path：生成する画像の保存先となるディレクトリ名
+mix：連続する画像を重ね合わせる
 ```
 実行コマンド例
+入力画像の色相を８通りに変化させる
 ```
 python3 colorful.py test.jpg 8 images
+```
+入力画像の色相を8通りに変化させ、それぞれを重ね合わせた中間の画像も生成
+```
+python3 colorful.py test.jpg 8 mix_images --mix
 ```
--- colorful.py
+++ colorful.py
@@ -4,11 +4,20 @@ import cv2
 import numpy as np
 import argparse
 
+def mix(img1, img2) :
+    beta  = 0.5
+    alpha = 1 - beta
+    mixed = cv2.addWeighted(src1=img1, alpha=alpha,
+                            src2=img2, beta=beta,
+                            gamma=0)
+    return mixed
+
 def main():
     parser = argparse.ArgumentParser(description = "TrainArgs")
-    parser.add_argument('file',      type=str, help='path to image file')
-    parser.add_argument('num_img',   type=int, default=16,  help='number of result files')
-    parser.add_argument('save_path', type=str, default="save_path", help="")
+    parser.add_argument('file', type=str, help='path to image file')
+    parser.add_argument('num_img', type=int, default=16,  help='number of result files')
+    parser.add_argument('save_path', type=str, default="save_path", help="path to save directory")
+    parser.add_argument('--mix', action='store_true')
     args = parser.parse_args()
 
     # Make empty directory
@@ -37,6 +46,11 @@ def main():
     for i, data in enumerate(l_img) :
         save_name = "./"+args.save_path+"/"+str(i).zfill(3)+"_"+args.file
         cv2.imwrite(save_name, data, [cv2.IMWRITE_JPEG_QUALITY,100])
+        if args.mix :
+            next_data = l_img[i+1] if i != len(l_img)-1 else l_img[0]
+            save_name = "./"+args.save_path+"/"+str(i).zfill(3)+"-mix_"+args.file
+            mix_img = mix(data, next_data) 
+            cv2.imwrite(save_name, mix_img, [cv2.IMWRITE_JPEG_QUALITY,100])
 
 if __name__ == '__main__' :
     main()
